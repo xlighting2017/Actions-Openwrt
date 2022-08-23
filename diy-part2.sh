168780644b1d4a8dcce0e41228978bb380016dbb
xlighting2017 <xlighting@gmail.com>
2022-08-23 23:49:03 +0800

    Update diy-part2.sh
    fix git url of openclash

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,14 +36,14 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 
 
 #使用原始最新版本，而不是上游package里面的版本
-git clone https://github.com/vernesong/OpenClash/luci-app-openclash.git package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 # git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+
 
 #VSSR
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
