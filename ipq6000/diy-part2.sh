fffee02a57241d550bde73e04b687e3ce9c3c2e1
xlighting2017 <xlighting@gmail.com>
2024-02-13 10:44:16 +0800

    Update diy-part2.sh

--- ipq6000/diy-part2.sh
+++ ipq6000/diy-part2.sh
@@ -37,10 +37,20 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 # 使用原始最新版本，而不是上游package里面的
 rm -rf ./feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
 rm -rf ./feeds/packages/smartdns
-git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone --filter=blob:none --sparse https://github.com/immortalwrt/packages
+cd packages
+git sparse-checkout add net/smartdns
+cp -Rf net/smartdns package/smartdns
+cd ..
+
 rm -rf ./feeds/luci/applications/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone --filter=blob:none --sparse https://github.com/immortalwrt/luci
+cd luci
+git sparse-checkout add applications/luci-app-smartdns
+cp -Rf applications/luci-app-smartdns package/luci-app-smartdns
+cd ..
 
 
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
