250df43b8ddfa0718321f932c59f85722d703011
xlighting2017 <xlighting@gmail.com>
2024-02-19 21:07:48 +0800

    Update diy-part2.sh

--- mt798x/diy-part2.sh
+++ mt798x/diy-part2.sh
@@ -36,7 +36,8 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65536' package
 
 # 使用原始最新版本，而不是上游package里面的
 rm -rf ./feeds/luci/applications/luci-app-openclash
-git clone --depth=1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
+git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
+
 # rm -rf ./feeds/luci/applications/luci-app-openclash
 # git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # rm -rf ./feeds/packages/smartdns
