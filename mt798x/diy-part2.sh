f0a5ff10a1315e3261b82514e03fa39a9456fb44
xlighting2017 <xlighting@gmail.com>
2024-02-28 14:00:23 +0800

    Update diy-part2.sh

--- mt798x/diy-part2.sh
+++ mt798x/diy-part2.sh
@@ -38,7 +38,8 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65536' package
 rm -rf ./feeds/luci/applications/luci-app-openclash
 git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
+git clone --depth=1 -b v5 https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+./scripts/feeds install -a -f
 
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
