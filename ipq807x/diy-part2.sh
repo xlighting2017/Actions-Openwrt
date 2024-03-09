1bd561a14f46898b025bd335763baffdca5f7b7f
xlighting2017 <xlighting@gmail.com>
2024-03-09 11:55:02 +0800

    ipq807x: add mosdns

--- ipq807x/diy-part2.sh
+++ ipq807x/diy-part2.sh
@@ -35,8 +35,10 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65536' package
 # sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 使用原始最新版本，而不是上游package里面的
-# rm -rf ./feeds/luci/applications/luci-app-openclash
-# git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-openclash
+git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
+
+git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
 
 
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
