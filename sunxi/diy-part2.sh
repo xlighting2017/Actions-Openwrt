9f402ce16ecec2d418eba73701793f5ccc53818a
xlighting2017 <xlighting@gmail.com>
2024-03-09 11:52:00 +0800

    sunxi: add mosdns

--- sunxi/diy-part2.sh
+++ sunxi/diy-part2.sh
@@ -46,8 +46,11 @@ echo 'net.netfilter.nf_conntrack_max=165535' >> package/base-files/files/etc/sys
 
 
 # 使用原始最新版本，而不是上游package里面的
-# rm -rf ./feeds/luci/applications/luci-app-openclash
-# git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-openclash
+git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
+
+git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+
 
 # rm -rf ./feeds/packages/smartdns
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
