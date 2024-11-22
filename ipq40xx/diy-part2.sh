96a220c4b9abe24755718dcd01f24395294e3322
xlighting2017 <xlighting@gmail.com>
2024-11-22 10:33:21 +0800

    Update diy-part2.sh

--- ipq40xx/diy-part2.sh
+++ ipq40xx/diy-part2.sh
@@ -17,8 +17,8 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 echo 'net.netfilter.nf_conntrack_max=65536' >> package/base-files/files/etc/sysctl.d/10-default.conf
 
 # 使用原始最新版本，而不是上游package里面的
-rm -rf ./feeds/luci/applications/luci-app-openclash
-git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
+# rm -rf ./feeds/luci/applications/luci-app-openclash
+# git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
 
 # add mosdns
 git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
