42d50672e2ab313ce7519f684399bfad684d4c36
xlighting2017 <xlighting@gmail.com>
2024-11-26 10:04:20 +0800

    Update diy-part2.sh

--- AX6.with.ECM/diy-part2.sh
+++ AX6.with.ECM/diy-part2.sh
@@ -21,7 +21,7 @@ echo 'net.netfilter.nf_conntrack_max=65536' >> package/base-files/files/etc/sysc
 # git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
 
 # add mosdns
-git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+git clone -b v5 https://github.com/xlighting2017/luci-app-mosdns.git package/mosdns
 
 # import cpufreq from immortalwrt
 git clone --filter=blob:none --sparse https://github.com/immortalwrt/immortalwrt
