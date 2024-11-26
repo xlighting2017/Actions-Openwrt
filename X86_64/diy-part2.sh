0fd810eae1669f6a7b46d2fd294737bd960bb486
xlighting2017 <xlighting@gmail.com>
2024-11-26 10:03:44 +0800

    Update diy-part2.sh

--- X86_64/diy-part2.sh
+++ X86_64/diy-part2.sh
@@ -21,4 +21,4 @@ echo 'net.netfilter.nf_conntrack_max=65536' >> package/base-files/files/etc/sysc
 # git clone --depth=1 -b patch-1 https://github.com/xlighting2017/OpenClash.git package/luci-app-openclash
 
 # add mosdns
-git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+git clone -b v5 https://github.com/xlighting2017/luci-app-mosdns.git package/mosdns
